2d08b16aa35a4a6aacd31a95810764d9551207f3
amit-sgwn <amitsangwan898@gmail.com>
2017-01-07 16:09:37 +0530

    added method for models

--- Book/models.py
+++ Book/models.py
@@ -13,5 +13,18 @@ class Book(models.Model):
     author = models.CharField(max_length=50)
     tag = models.TextField()
     
+    def __str__(self):
+        return self.book_name
+    
+    def num_of_copies(self):
+        return self.no_of_coies
+    
+    def sub(self):
+        return self.book_subject
+    
 class Subject(models.Model):
     sub_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.sub_name
+    
