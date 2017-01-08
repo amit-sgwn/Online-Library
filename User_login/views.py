d5368c14d904d05bbb173493d0f9ae7ccd4d1e08
amit-sgwn <amitsangwan898@gmail.com>
2017-01-08 12:25:19 +0530

    Amodified settings.py  and addd installed app

--- User_login/views.py
+++ User_login/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.conntrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url="login/")
+#@login_required(login_url="login/")
 def home(request):
     return render(request,"home.html")
